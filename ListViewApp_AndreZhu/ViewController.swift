0c8bc4d091ceaa9c21e80c189333019709db6ad2
Andre  Zhu <azhu1995@atc203-m05.local>
2017-05-08 19:07:14 -0700

    Initial Commit

---
+++ ListViewApp_AndreZhu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListViewApp_AndreZhu
+//
+//  Created by Andre  Zhu on 5/8/17.
+//  Copyright © 2017 Andre  Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
