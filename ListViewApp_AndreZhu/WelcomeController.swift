efbf6a612e4365f2b124eaef4b5d085cd8feebca
robbynblume <shiningflower777@gmail.com>
2017-06-19 16:23:58 -0700

    changed about page and tabs to correct names

---
+++ ListViewApp_AndreZhu/WelcomeController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeController.swift
+//  ListViewApp_AndreZhu
+//
+//  Created by Robbyn Blumenschein on 6/19/17.
+//  Copyright © 2017 Andre  Zhu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+    @IBOutlet var WelcomeLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
